2d886ae3cde4569046cc4ec7c1da7dc605a15454
“harshaV87” <harsha.87b@gmail.com>
2023-08-29 16:19:42 -0400

    Initial Commit

---
+++ OpinionAnalysis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpinionAnalysis
+//
+//  Created by Venkata harsha Balla on 8/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpinionAnalysis/OpinionAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpinionAnalysisApp.swift
+//  OpinionAnalysis
+//
+//  Created by Venkata harsha Balla on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct OpinionAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
